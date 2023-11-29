ccf2e563adf3d7c19da0b7c0f2c03b0975eb63a2
isa04389dora <136838389+isa04389dora@users.noreply.github.com>
2023-11-29 13:55:56 -0300

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,19 @@
+var slide = document.querySelector('.slideshow');
+var images = [
+    './img/banner.png' ,
+    './img/banner2.jpg',
+    
+];
+var time = 2000;
+var count = 0;
+
+function moveSlideShow(){
+    slide.src = images[count];
+    if (count < images.length - 1){
+            count++;
+        }else{
+            count=0;
+        }
+        setTimeout("moveSlideShow()", time);
+    }
+    window.onload = moveSlideShow;
